49e61bbcd7d1e8cd8a9d9cea8f3b013c02cd73d0
hoyinm14mc <mail@hoyinm14mc.com>
2016-07-28 00:28:52 +0800

    Did some work for bail feature implementation

---
+++ src/hoyinm14mc/jail/economy/Economyapi.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * This file is a part of Jail.
+ * Copyright (C) 2016 hoyinm14mc
+ *
+ * Jail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jail. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace hoyinm14mc\jail\economy;
+
+use hoyinm14mc\jail\base\BaseEconomy;
+use pocketmine\Player;
+
+class Economyapi extends BaseEconomy
+{
+
+    public function bail(Player $player): bool
+    {
+        if ($this->getPlugin()->isJailed($player->getName()) !== true) {
+            $player->sendMessage($this->getPlugin()->colorMessage("&cYou are not jailed!"));
+            return false;
+        }
+        $t = $this->getPlugin()->data->getAll();
+        $money = $this->getPlugin()->getEco()->getInstance()->myMoney($player);
+        if ($money < ($t[$player->getName()]["seconds"] * $this->getPlugin()->getConfig()->get("bail-per-second") + 1)) {
+            $player->sendMessage($this->getPlugin()->colorMessage("&cYou don't have enough money to bail!\n&cYou need " . ($t[$player->getName()]["seconds"] * ($this->getPlugin()->getConfig()->get("bail-per-second")) + 1)));
+            return false;
+        }
+        $this->getPlugin()->getEco()->getInstance()->reduceMoney($player, ($t[$player->getName()]["seconds"] * ($this->getPlugin()->getConfig()->get("bail-per-second")) + 1));
+        $this->getPlugin()->unjail($player);
+        $player->sendMessage($this->getPlugin()->colorMessage("&aYou have been unjailed successfully!"));
+        $player->sendMessage("Bank : -$" . ($t[$player->getName()]["seconds"] * ($this->getPlugin()->getConfig()->get("bail-per-second")) + 1) . " | $" . $this->getPlugin()->getEco()->getInstance()->myMoney($player) . " remaining in your account");
+        return true;
+    }
+
+}
+
+?>
