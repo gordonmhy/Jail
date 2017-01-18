323fdb899e87850b5485bf23d9e6855d69a2e61a
hoyinm14mc <hoyinm14@gmail.com>
2017-01-18 23:11:30 +0800

    Added ResetMine sign

---
+++ src/hoyinm14mc/jail/listeners/sign/ResetmineListener.php
@@ -0,0 +1,142 @@
+<?php
+/*
+ * This file is a part of Jail.
+ * Copyright (C) 2017 hoyinm14mc
+ *
+ * Jail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jail. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace hoyinm14mc\jail\listeners\sign;
+
+use hoyinm14mc\jail\base\BaseListener;
+use hoyinm14mc\jail\Mines;
+use pocketmine\block\Block;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\tile\Sign;
+
+class ResetmineListener extends BaseListener
+{
+
+    public function onBlockBreak(BlockBreakEvent $event)
+    {
+        if ($event->getBlock()->getID() != 323 && $event->getBlock()->getID() != 63 && $event->getBlock()->getID() != 68) {
+            return false;
+        }
+        $sign = $event->getPlayer()->getLevel()->getTile($event->getBlock());
+        if ($sign instanceof Sign !== true) {
+            return false;
+        }
+        $sign = $sign->getText();
+        if ($sign[0] == $this->getPlugin()->colorMessage("&7[" . $this->getPlugin()->getMessage("sign.resetmine.tag") . "&7]")) {
+            if ($event->getPlayer()->hasPermission("jail.sign.destroy") !== false && $this->getPlugin()->isJailed(strtolower($event->getPlayer()->getName())) !== true) {
+                $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("sign.destroy.success"));
+                return true;
+            } else {
+                $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("no.permission"));
+                $event->setCancelled(true);
+            }
+        }
+    }
+
+    public function onSignChange(SignChangeEvent $event)
+    {
+        if ($event->getBlock()->getID() != 323 && $event->getBlock()->getID() != 63 && $event->getBlock()->getID() != 68) {
+            return false;
+        }
+        $sign = $event->getPlayer()->getLevel()->getTile($event->getBlock());
+        if ($sign instanceof Sign !== true) {
+            return false;
+        }
+        $sign = $event->getLines();
+        if ($sign[0] != "[ResetMine]") {
+            return false;
+        }
+        if ($event->getPlayer()->hasPermission("jail.sign.create") !== true) {
+            $event->setLine(0, null);
+            $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("no.permission"));
+            return false;
+        }
+        $j = $this->getPlugin()->data1->getAll();
+        $inside = false;
+        $ja = "";
+        foreach (array_keys($j) as $jail) {
+            if ($this->getPlugin()->insideJail($jail, new Position($event->getBlock()->x, $event->getBlock()->y, $event->getBlock()->z, $event->getBlock()->getLevel())) !== false) {
+                $inside = true;
+                $ja = $jail;
+            }
+        }
+        if ($inside !== true) {
+            $event->getBlock()->getLevel()->setBlock(new Vector3($event->getBlock()->x, $event->getBlock()->y, $event->getBlock()->z), Block::get(0));
+            $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("sign.located.outsideJail"));
+            return false;
+        }
+        $mines = new Mines($this->getPlugin());
+        if ($mines->hasMineSet($ja) !== true) {
+            $event->getBlock()->getLevel()->setBlock(new Vector3($event->getBlock()->x, $event->getBlock()->y, $event->getBlock()->z), Block::get(0));
+            $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("mine.not.set"));
+            return false;
+        }
+        $event->setLine(0, $this->getPlugin()->colorMessage("&7[" . $this->getPlugin()->getMessage("sign.resetmine.tag") . "&7]"));
+        $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("sign.create.success"));
+        return true;
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $event)
+    {
+        if ($event->getBlock()->getID() != 323 && $event->getBlock()->getID() != 63 && $event->getBlock()->getID() != 68) {
+            return false;
+        }
+        $sign = $event->getPlayer()->getLevel()->getTile($event->getBlock());
+        if ($sign instanceof Sign !== true) {
+            return false;
+        }
+        $sign = $sign->getText();
+        if ($sign[0] == $this->getPlugin()->colorMessage("&7[" . $this->getPlugin()->getMessage("sign.resetmine.tag") . "&7]")) {
+            if ($this->getPlugin()->isJailed($event->getPlayer()->getName()) !== true) {
+                $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("sign.not.jailed"));
+                return false;
+            }
+            if ($event->getPlayer()->hasPermission("jail.sign.use") !== true) {
+                $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("no.permission"));
+                return false;
+            }
+            $j = $this->getPlugin()->data1->getAll();
+            $ja = "";
+            foreach (array_keys($j) as $jail) {
+                if ($this->getPlugin()->insideJail($jail, new Position($event->getBlock()->x, $event->getBlock()->y, $event->getBlock()->z, $event->getBlock()->getLevel())) !== false) {
+                    $ja = $jail;
+                }
+            }
+            $mines = new Mines($this->getPlugin());
+            if ($mines->hasMineSet($ja) !== true) {
+                $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("mine.not.set"));
+                return false;
+            }
+            $t = $this->getPlugin()->data->getAll();
+            if ($t[$event->getPlayer()->getName()]["jail"] != $ja) {
+                $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("sign.not.inJail"));
+                return false;
+            }
+            $this->getPlugin()->getServer()->dispatchCommand($event->getPlayer(), "jailresetmine");
+            return true;
+        }
+    }
+
+}
+
+?>
