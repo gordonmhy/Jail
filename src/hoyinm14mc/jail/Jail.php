d26a7cf7ebadfa6b82b6e98b29e4101dba3daf67
hoyinm14mc <hoyinm14@gmail.com>
2017-01-20 17:49:23 +0800

    Local To Database synchronization

--- src/hoyinm14mc/jail/Jail.php
+++ src/hoyinm14mc/jail/Jail.php
@@ -63,6 +63,11 @@ class Jail extends PluginBase implements JailAPI
     public $database = false;
 
     /**
+     * @var \mysqli
+     */
+    public $connection;
+
+    /**
      * @var array:array
      */
     public $c1_tmp = [];
@@ -146,13 +151,19 @@ class Jail extends PluginBase implements JailAPI
             $this->saveDefaultConfig();
         }
         $this->reloadConfig();
+        $this->data = new Config($this->getDataFolder() . "players.yml", Config::YAML, array());
+        $this->data1 = new Config($this->getDataFolder() . "jails.yml", Config::YAML, array());
         $database = false;
         if ($this->initializeDatabase() !== false) {
             $database = true;
         }
         $this->database = $database;
-        $this->data = new Config($this->getDataFolder() . "players.yml", Config::YAML, array());
-        $this->data1 = new Config($this->getDataFolder() . "jails.yml", Config::YAML, array());
+        if ($this->database !== false && $this->getConfig()->get("syncLOCALtoDB") !== false) {
+            $this->synchronizeToDatabase();
+        }
+        if ($this->database !== false && $this->getConfig()->get("syncDBtoLOCAL") !== false) {
+            $this->synchronizeToLocal();
+        }
         $this->initializeLanguage();
         $ecoPlugs = [
             "PocketMoney",
@@ -202,6 +213,13 @@ class Jail extends PluginBase implements JailAPI
             } else {
                 $this->getServer()->getScheduler()->scheduleAsyncTask(new AsyncUpdateChecker());
             }
+        }
+    }
+
+    public function onDisable()
+    {
+        if ($this->database !== false) {
+            $this->connection->close();
         }
     }
 
@@ -600,8 +618,31 @@ class Jail extends PluginBase implements JailAPI
             $this->getLogger()->info("SQL Server connection error: " . $connection->connect_error);
             return false;
         }
+        $this->connection = $connection;
         $this->getLogger()->info("SQL server connection detected!");
         return true;
+    }
+
+    //Database synchronization task 1 (per 15 minutes)
+    private function synchronizeToDatabase()
+    {
+        //Table name = JailData01
+        $this->connection->query("DROP TABLE JailData01");
+        $this->connection->query("CREATE TABLE JailData01 
+                                  (Jail CHAR(25) PRIMARY KEY, PosX INTEGER, PosY INTEGER, PosZ INTEGER, PosLevel CHAR(25), C1X INTEGER, C1Y INTEGER, C1Z INTEGER, C1Level CHAR(25), C2X INTEGER, C2Y INTEGER, C2Z INTEGER, C2Level CHAR(25), AllowBail BOOLEAN, AllowEscapeArea BOOLEAN, MineIsSet BOOLEAN, MineC1X INTEGER, MineC1Y INTEGER, MineC1Z INTEGER, MineC2X INTEGER, MineC2Y INTEGER, MineC2Z INTEGER)");
+        $j = $this->data1->getAll();
+        foreach (array_keys($j) as $jail) {
+            $this->connection->query("INSERT INTO JailData01 
+                                      (Jail, PosX, PosY, PosZ, PosLevel, C1X, C1Y, C1Z, C1Level, C2X, C2Y, C2Z, C2Level, AllowBail, AllowAreaEscape, MineIsSet, MineC1X, MineC1Y, MineC1Z, MineC2X, MineC2Y, MineC2Z) 
+                                      VALUES 
+                                      ('" . $jail . "', " . $j[$jail]["pos"]["x"] . ", " . $j[$jail]["pos"]["y"] . ", " . $j[$jail]["pos"]["z"] . ", " . $j[$jail]["pos"]["level"] . ", " . $j[$jail]["c1"]["x"] . ", " . $j[$jail]["c1"]["y"] . ", " . $j[$jail]["c1"]["z"] . ", " . $j[$jail]["c1"]["level"] . ", " . $j[$jail]["c2"]["x"] . ", " . $j[$jail]["c2"]["y"] . ", " . $j[$jail]["c2"]["z"] . ", " . $j[$jail]["c2"]["level"] . ", " . (bool)$j[$jail]["allow-bail"] . ", " . (bool)$j[$jail]["allow-escape-area"] . ", " . (bool)$j[$jail]["mine"]["isSet"] . ", " . $j[$jail]["mine"]["c1"]["x"] . ", " . $j[$jail]["mine"]["c1"]["y"] . ", " . $j[$jail]["mine"]["c1"]["z"] . ", " . $j[$jail]["mine"]["c2"]["x"] . ", " . $j[$jail]["mine"]["c2"]["y"] . ", " . $j[$jail]["mine"]["c2"]["z"] . ")");
+        }
+    }
+
+    //Database synchronization task 2 (per 15 minutes)
+    private function synchronizeToLocal()
+    {
+
     }
 
 }
