1953670e1c937455523f3a58cb658833ce319ae0
hoyinm14mc <hoyinm.dev@gmail.com>
2017-02-25 17:21:32 +0800

    Old config file(s) is now saved with old version name

--- src/hoyinm14mc/jail/Jail.php
+++ src/hoyinm14mc/jail/Jail.php
@@ -144,12 +144,13 @@ class Jail extends PluginBase implements JailAPI
         }
         $this->saveDefaultConfig();
         if ($this->getConfig()->exists("v") !== true || $this->getConfig()->get("v") != $this->getDescription()->getVersion()) {
+            $oldV = $this->getConfig()->get("v");
             $this->getLogger()->info("Update found!  Updating configuration...");
             $this->getLogger()->info("All settings are being reset. The old config is saved in the plugin data folder.");
-            if (file_exists($this->getDataFolder() . "config.yml.old")) {
-                unlink($this->getDataFolder() . "config.yml.old");
+            if (file_exists($this->getDataFolder() . "config.old.v" . $oldV . "yml") !== false) {
+                unlink($this->getDataFolder() . "config.old.v" . $oldV . "yml");
             }
-            rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "config.yml.old");
+            rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "config.old.v" . $oldV . "yml");
             $this->saveDefaultConfig();
         }
         $this->reloadConfig();
