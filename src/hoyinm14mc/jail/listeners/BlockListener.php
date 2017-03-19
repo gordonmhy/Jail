e17d119bc4e39233015bdff9ba118c1bfe74ebea
hoyinm14mc <hoyinm.dev@gmail.com>
2017-03-19 17:43:24 +0800

    Fixed prisoners able to break blocks when mine is set #22

--- src/hoyinm14mc/jail/listeners/BlockListener.php
+++ src/hoyinm14mc/jail/listeners/BlockListener.php
@@ -41,7 +41,14 @@ class BlockListener extends BaseListener
             }
         }
         if ($this->getPlugin()->isJailed(strtolower($event->getPlayer()->getName())) !== false && $cfg->get("allow-block-break") !== true) {
-            if ($mines->hasMineSet($t[strtolower($event->getPlayer()->getName())]["jail"]) !== true && $mines->insideMine($t[strtolower($event->getPlayer()->getName())]["jail"], new Position($event->getBlock()->x, $event->getBlock()->y, $event->getBlock()->z, $event->getBlock()->getLevel())) !== true && $event->getPlayer()->hasPermission("jail.override.restrictions") !== true) {
+            if ($mines->hasMineSet($t[strtolower($event->getPlayer()->getName())]["jail"]) !== true && $event->getPlayer()->hasPermission("jail.override.restrictions") !== true) {
+                $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("listener.not.allowed.do.this"));
+                $event->setCancelled(true);
+                if ($cfg->get("enable-penalty") !== false && isset($t[strtolower($event->getPlayer()->getName())]["seconds"])) {
+                    $this->getPlugin()->applyPenalty(strtolower($event->getPlayer()->getName()), (int)$cfg->get("penalty-time"));
+                    $event->getPlayer()->sendMessage(str_replace("%time%", $cfg->get("penalty-time"), $this->getPlugin()->getMessage("penalty.added.prisoner")));
+                }
+            } else if ($mines->hasMineSet($t[strtolower($event->getPlayer()->getName())]["jail"]) !== false && $mines->insideMine($t[strtolower($event->getPlayer()->getName())]["jail"], new Position($event->getBlock()->x, $event->getBlock()->y, $event->getBlock()->z, $event->getBlock()->getLevel())) !== true && $event->getPlayer()->hasPermission("jail.override.restrictions") !== true) {
                 $event->getPlayer()->sendMessage($this->getPlugin()->getMessage("listener.not.allowed.do.this"));
                 $event->setCancelled(true);
                 if ($cfg->get("enable-penalty") !== false && isset($t[strtolower($event->getPlayer()->getName())]["seconds"])) {
