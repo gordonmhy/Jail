f62793795d0f9a0fe1368fe535aa6e38bd9dadd4
piyushbest <42518831+piyushbest@users.noreply.github.com>
2018-12-16 11:02:48 +0530

    Update Jail.php

--- src/hoyinm14mc/jail/Jail.php
+++ src/hoyinm14mc/jail/Jail.php
@@ -17,7 +17,7 @@
  * along with Jail. If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace hoyinm14mc\jail;
+namespace hoyinm14mc\escape;
 
 use hoyinm14mc\jail\commands\BailCommand;
 use hoyinm14mc\jail\commands\DeljailCommand;
